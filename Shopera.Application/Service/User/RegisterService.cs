dc63bc52308d4937aceeebeed686f0aac6154e04
zakie78.ashoori@gmail.com <zakie78.ashoori@gmail.com>
2026-07-10 12:07:41 +0430

    .

---
+++ Shopera.Application/Service/User/RegisterService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Shopera.Application.IService;
+
+namespace Shopera.Application.Service.User
+{
+    private readonly UserManager<ApplicationUser> userManager;
+
+    class RegisterService(userManager) : IRegisterService
+    {
+
+    }
+}
